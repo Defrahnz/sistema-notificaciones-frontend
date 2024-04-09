ef3c846e902cc7ae3fce9a62edd3b7a88bfd170b
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-04-08 23:34:42 -0600

    Se puso la funcionalidad de los nuevos campos, así como métodos para generar los nombres de usuario automáticamente

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
@@ -21,28 +23,55 @@ export class RegistroComponent implements OnInit {
     telefonoCasa:'',
     telefonoMovil:'',
     email:'',
-    password:''
+    password:'',
+    rol:'',
+    username:''
   }
-  constructor(private userService:UserService){}
-  ngOnInit():void{}
+  public roles:any[]=[];
+  constructor(private userService:UserService, private toast:ToastrService){}
+  ngOnInit():void{
+    this.obtenerRoles();
+    this.generarUsername();
+  }
 
 
   formSubmit(){
     console.log(this.user)
     if(this.user.email =='' || this.user.email==null){
-      alert('El correo es requerido');
+      this.toast.error("El correo electrónico es requerido","Error de validación");
       return;
     }
 
     this.userService.registrarUsuario(this.user).subscribe(
       (data)=>{
         console.log(data);
-        alert('Usuario guardado correctamente')
+        Swal.fire('¡Registrado!','Se ha registrado correctamente el usuario','success')
       },(error=>{
         console.log(error);
-        alert('Ha ocurrido un error. Intente de nuevo')
+        this.toast.error("Ocurrió un error al registrarse","Error de servidor");
       })
-    )
+    )}
 
-  }
+    obtenerRoles(){
+      this.userService.obtenerRoles().subscribe(
+        (data:any)=>{
+          this.roles=data;
+        },
+        error =>{
+          console.log(error);
+          this.toast.error('Ocurrió un error al obener los roles','Error de Servidor')
+        });
+    }
+
+    generarUsername(){
+      if(this.user.apellidoP && this.user.apellidoM && this.user.nombre){
+        const apellidoP=this.user.apellidoP.substring(0,3).toLowerCase();
+        const apellidoM=this.user.apellidoM.substring(0,1).toLowerCase();
+        const nombre=this.user.nombre.substring(0,2).toLowerCase();
+        const aleatorio=Math.floor(Math.random()*101);
+        this.user.username=`${apellidoP}${apellidoM}${nombre}${aleatorio}`;
+      }else{
+        console.log("Se están entrando los campos");
+      }
+    }
 }
