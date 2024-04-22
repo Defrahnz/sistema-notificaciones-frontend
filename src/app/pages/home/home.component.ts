753ba0b72d5beda925f9c639ca3930a3b876296e
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-04-22 01:45:32 -0600

    Se agregó el código correspondiente para mostrar algo en el componente de Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModuloService } from 'src/app/services/modulo.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  modulos:any[]=[];
+  constructor(private moduloService:ModuloService){}
 
+  ngOnInit(): void {
+      this.moduloService.obtenerModulosActivos().subscribe(
+        (data:any)=>{
+          this.modulos = data;
+        },
+        error =>{
+          console.log("Ocurrio un error al obtener los módulos");
+        });
+  }
 }
